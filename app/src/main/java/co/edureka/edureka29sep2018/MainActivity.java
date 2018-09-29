ed5dec9eb6337c068854d7ca10acf1d2d1adf566
Ishant Kumar <er.ishant@gmail.com>
2018-09-29 09:49:01 +0530

    Initial commit

---
+++ app/src/main/java/co/edureka/edureka29sep2018/MainActivity.java
@@ -0,0 +1,99 @@
+package co.edureka.edureka29sep2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Date;
+
+// AppCompatActivity is an API to create Activity i.e. UI
+public class MainActivity extends AppCompatActivity {
+
+
+    // onCreate is an overrided method which serves as a constructor to us
+    // is executed when object of activity is created by Android System
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); // calling parent's method ? Parent will maintain state of Activity
+
+        // Layout is set on Activity
+        setContentView(R.layout.activity_main);
+        Log.i("MainActivity","==onCreate==");
+
+        //System.out.print("MainActivity - onCreate");
+    }
+
+
+    // onStart is executed when Object of Activity is in the Task
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("MainActivity","==onStart==");
+    }
+
+    // onResume is a state when Activity will become Visible to User and User is interacting with it
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("MainActivity","==onResume==");
+    }
+
+    // Use Case 1 ** Whenever a new Activity is launched -> onCreate -> onStart -> onResume **
+
+    // Use Case 2 ** When a new Activity2 is launched from existing Activity1, and it covers Activity1 fully **
+    //            ** Activity goes in background
+    //            ** Activity1 -> onCreate -> onStart -> onResume | Activity2 -> onCreate -> onStart -> onResume
+    //            ** Activity2 covered Activity1 fully !!
+    //            ** Activity1 -> onPause -> onStop
+
+    // Use Case 3 ** When a new Activity2 is launched from existing Activity1, and it covers Activity1 partially **
+    //            ** Activity1 -> onCreate -> onStart -> onResume | Activity2 -> onCreate -> onStart -> onResume
+    //            ** Activity2 covered Activity1 partially !!
+    //            ** Activity1 -> onPause
+
+    // Use Case 4 ** Whenever user presses back key to come out of Activity
+    //            ** onCreate -> onStart -> onResume
+    //            ** onPause -> onStop -> onDestroy
+
+
+    // When Activity is fully covered by some other Activity
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("MainActivity","==onStop==");
+    }
+
+    // When Activity is partially covered by some other Activity
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("MainActivity","==onPause==");
+    }
+
+    // When user presses the back key or we execute finish() method in our code
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("MainActivity","==onDestroy==");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("MainActivity","==onRestart==");
+    }
+
+    // Event Handling - 1
+    // method name can be any name of your choice !! eg: clickHandler here
+    // We need to pass an input i.e. View API
+    public void clickHandler(View view){
+
+        Date date = new Date();
+        String dateTime = date.toString();
+
+        Toast.makeText(this,"Today is: "+dateTime,Toast.LENGTH_LONG).show();
+
+    }
+}
